2eba1bd87b25b2ff2440acad86e6e7d6482db175
Sherif Ashraf <sherifashraf1@hotmail.com>
2021-07-31 17:48:57 +0200

    upload version 8.2.1 build 2 to appstore

--- WeasyVendor/Controllers/APIConnection.swift
+++ WeasyVendor/Controllers/APIConnection.swift
@@ -10,14 +10,13 @@ import Foundation
 import Alamofire
 
 class APIConnection{
+    
     static var apiConnection = APIConnection()
     
     func getConnection (completion: @escaping(Int,NSDictionary,String)-> (), link : String){
-        
-        if ReachabilityManager.shared.isConnected() {
-            Alamofire.request(link, method: .get, headers: Constants.HEADER).responseJSON { response in
-                if let JSON = response.result.value  as? NSDictionary{
-                    if let msg = JSON["msg"] as? String {
+        Alamofire.request(link, method: .get, headers: Constants.HEADER).responseJSON { response in
+            if let JSON = response.result.value  as? NSDictionary{
+                if let msg = JSON["msg"] as? String {
                     if let check = JSON["status_code"] as? Int{
                         if check == 200 {
                             if  let data = JSON["data"] as? NSDictionary{
@@ -33,45 +32,35 @@ class APIConnection{
                         }
                     }
                 }
-                }
             }
-        } else {
-            completion(2, [:],"No Internet Connection".localized)
-            
         }
     }
     
     func postConnection (completion: @escaping(Int,NSDictionary,String)-> (), link : String, param: Parameters ){
-        
-        if ReachabilityManager.shared.isConnected() {
-            print(param)
-            Alamofire.request(link, method: .post, parameters: param, headers: Constants.HEADER).responseJSON { response in
-                print(response.result.value)
-                print(link)
-                print(Constants.HEADER["token"])
-                if let JSON = response.result.value  as? NSDictionary{
-                    print(JSON)
-                    if let msg = JSON["msg"] as? String {
-                        
-                        if let check = JSON["status_code"] as? Int{
-                            if check == 200 {
-                                if let data = JSON["data"] as? NSDictionary{
-                                    completion(0, data,msg )
-                                }
-                                else{
-                                    completion(0, [:],msg )
-                                }
+        print(param)
+        Alamofire.request(link, method: .post, parameters: param, headers: Constants.HEADER).responseJSON { response in
+            print(response.result.value)
+            print(link)
+            print(Constants.HEADER["token"])
+            if let JSON = response.result.value  as? NSDictionary{
+                print(JSON)
+                if let msg = JSON["msg"] as? String {
+                    
+                    if let check = JSON["status_code"] as? Int{
+                        if check == 200 {
+                            if let data = JSON["data"] as? NSDictionary{
+                                completion(0, data,msg )
                             }
                             else{
-                                completion(1, [:],msg)
+                                completion(0, [:],msg )
                             }
+                        }
+                        else{
+                            completion(1, [:],msg)
                         }
                     }
                 }
             }
-        }
-        else {
-            completion(2, [:],"No Internet Connection".localized)
         }
     }
     
